7ff04e7254466a72b3c3402e9a2e102337d04eff
Ryan <ryanmiller732@gmail.com>
2023-10-15 12:04:45 -0400

    docs: until param and return documentation

--- utils/async/src/until.ts
+++ utils/async/src/until.ts
@@ -1,8 +1,16 @@
 // Src: https://blog.openreplay.com/forever-functional-waiting-with-promises
+
+/**
+ *
+ * @param fn A function that returns a promise or a boolean value
+ * @param delay The delay between each attempt
+ * @param maxWait The maximum time to wait before rejecting the promise
+ * @returns A promise that resolves when the function returns true
+ */
 export const until = async (
   fn: () => Promise<boolean> | boolean,
-  time: number = 1000,
-  wait: number = 10000
+  delay: number = 1000,
+  maxWait: number = 10000
 ) => {
   const startTime =
     new Date().getTime() /* 1. Store the starting time in case the test takes too long */
@@ -17,13 +25,13 @@ export const until = async (
       /* ...or if it throws an exception */
       throw e
     }
-    if (new Date().getTime() - startTime > wait) {
+    if (new Date().getTime() - startTime > maxWait) {
       throw new Error(
-        `Max wait reached. ${new Date().getTime() - startTime} > ${wait}`
+        `Max wait reached. ${new Date().getTime() - startTime} > ${maxWait}`
       ) /* 5. Check if enough time has passed; if so, throw an exception */
     } else {
       /* 6. Wait and test again */
-      await new Promise((resolve) => setTimeout(resolve, time))
+      await new Promise((resolve) => setTimeout(resolve, delay))
     }
   }
 }
