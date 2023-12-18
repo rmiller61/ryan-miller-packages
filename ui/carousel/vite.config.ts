a5bf8fd700b438955fc5903463550a5d0bad5649
Ryan <ryanmiller732@gmail.com>
2023-12-18 07:45:06 -0500

    config: add tsconfigPaths to vite config

--- ui/carousel/vite.config.ts
+++ ui/carousel/vite.config.ts
@@ -1,7 +1,14 @@
 import react from "@vitejs/plugin-react-swc"
+import * as path from "path"
 import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tsconfigPaths({
+      projects: [path.resolve(__dirname, "tsconfig.test.json")],
+    }),
+    react(),
+  ],
 })
