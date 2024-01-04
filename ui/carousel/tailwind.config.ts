7d4d8bca909bc5556f51fb959a47bfd1b9019489
Ryan <ryanmiller732@gmail.com>
2024-01-04 13:16:01 -0500

    chore: delete tailwind plugin

--- ui/carousel/tailwind.config.ts
+++ ui/carousel/tailwind.config.ts
@@ -1,8 +1,6 @@
-import plugin from "./tailwind.plugin"
 import type { Config } from "tailwindcss"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  plugins: [plugin],
 } satisfies Config
