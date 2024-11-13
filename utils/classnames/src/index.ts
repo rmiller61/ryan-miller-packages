b7fc8943d4fedb4f33f8583f9c223e896afec842
Ryan <ryanmiller732@gmail.com>
2024-11-12 19:47:56 -0500

    feat: remove default exports

--- utils/classnames/src/index.ts
+++ utils/classnames/src/index.ts
@@ -7,6 +7,6 @@ import { twMerge } from "tailwind-merge"
  * @param inputs Thank you Shad! https://ui.shadcn.com/docs/installation#add-a-cn-helper
  * @returns
  */
-export default function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
